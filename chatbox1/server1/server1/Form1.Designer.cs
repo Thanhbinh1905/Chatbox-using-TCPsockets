c713c498d42b5573803efd27dff63353155b2ec1
Trần Thanh Bình <91179512+Thanhbinh1905@users.noreply.github.com>
2023-04-09 12:43:12 +0700

    Add files via upload

---
+++ chatbox1/server1/server1/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace server1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClearMss = new System.Windows.Forms.Button();
+            this.Status = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSendMss = new System.Windows.Forms.Button();
+            this.txtbox = new System.Windows.Forms.TextBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnClearMss
+            // 
+            this.btnClearMss.Location = new System.Drawing.Point(515, 327);
+            this.btnClearMss.Name = "btnClearMss";
+            this.btnClearMss.Size = new System.Drawing.Size(75, 23);
+            this.btnClearMss.TabIndex = 15;
+            this.btnClearMss.Text = "Clear";
+            this.btnClearMss.UseVisualStyleBackColor = true;
+            this.btnClearMss.Click += new System.EventHandler(this.btnClearMss_Click);
+            // 
+            // Status
+            // 
+            this.Status.AutoSize = true;
+            this.Status.Location = new System.Drawing.Point(57, 19);
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(37, 13);
+            this.Status.TabIndex = 14;
+            this.Status.Text = "Offline";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Status:";
+            // 
+            // btnSendMss
+            // 
+            this.btnSendMss.Location = new System.Drawing.Point(434, 327);
+            this.btnSendMss.Name = "btnSendMss";
+            this.btnSendMss.Size = new System.Drawing.Size(75, 23);
+            this.btnSendMss.TabIndex = 12;
+            this.btnSendMss.Text = "Gui";
+            this.btnSendMss.UseVisualStyleBackColor = true;
+            this.btnSendMss.Click += new System.EventHandler(this.btnSendMss_Click);
+            // 
+            // txtbox
+            // 
+            this.txtbox.Location = new System.Drawing.Point(23, 327);
+            this.txtbox.Multiline = true;
+            this.txtbox.Name = "txtbox";
+            this.txtbox.Size = new System.Drawing.Size(401, 20);
+            this.txtbox.TabIndex = 11;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(604, 64);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 10;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(604, 35);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 9;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(23, 49);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(567, 264);
+            this.listBox1.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 406);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnClearMss);
+            this.Controls.Add(this.Status);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSendMss);
+            this.Controls.Add(this.txtbox);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClearMss;
+        private System.Windows.Forms.Label Status;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSendMss;
+        private System.Windows.Forms.TextBox txtbox;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ chatbox1/server1/server1/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace server1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            btnStop.Enabled = false;
+            btnSendMss.Enabled = false;
+            txtbox.Enabled = false;
+            btnClearMss.Enabled = false;
+        }
+
+        private TcpListener listener;
+        private List<TcpClient> clients = new List<TcpClient>();
+        private Thread listenThread;
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStop.Enabled = true;
+            btnSendMss.Enabled = true;
+            txtbox.Enabled = true;
+            btnClearMss.Enabled = true;
+
+            Status.Text = "Online";
+            listener = new TcpListener(IPAddress.Any, 1905);
+            listener.Start();
+
+            listenThread = new Thread(new ThreadStart(ListenForClients));
+            listenThread.Start();
+
+            listBox1.Items.Add("Server started");
+            btnStart.Enabled = false;
+        }
+
+        private void ListenForClients()
+        {
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                clients.Add(client);
+                Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClientComm));
+                clientThread.Start(client);
+            }
+        }
+
+        private void HandleClientComm(object client)
+        {
+            TcpClient tcpClient = (TcpClient)client;
+            NetworkStream clientStream = tcpClient.GetStream();
+
+            byte[] message = new byte[4096];
+            int bytesRead;
+
+            while (true)
+            {
+                bytesRead = 0;
+
+                try
+                {
+                    // read incoming message
+                    bytesRead = clientStream.Read(message, 0, 4096);
+                }
+                catch
+                {
+                    // a socket error has occurred
+                    break;
+                }
+
+                if (bytesRead == 0)
+                {
+                    // the client has disconnected from the server
+                    break;
+                }
+
+                // message received
+                string data = Encoding.ASCII.GetString(message, 0, bytesRead);
+                listBox1.Invoke(new Action(() => listBox1.Items.Add("Client: " + data)));
+            }
+
+            clients.Remove(tcpClient);
+            tcpClient.Close();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+            btnSendMss.Enabled = false;
+            txtbox.Enabled = false;
+            btnClearMss.Enabled = false;
+
+            Status.Text = "Offline";
+            listener.Stop();
+            listenThread.Abort();
+
+            foreach (TcpClient client in clients)
+            {
+                client.Close();
+            }
+
+            clients.Clear();
+
+            listBox1.Items.Add("Server stopped");
+            btnStop.Enabled = true;
+        }
+
+        private void btnSendMss_Click(object sender, EventArgs e)
+        {
+            string message = "Server: " + txtbox.Text;
+            string messagesend = txtbox.Text;
+            byte[] data = Encoding.ASCII.GetBytes(messagesend);
+
+            foreach (var client in clients)
+            {
+                client.GetStream().Write(data, 0, data.Length);
+            }
+
+            listBox1.Invoke(new Action(() => listBox1.Items.Add(message)));
+            txtbox.Clear();
+        }
+
+        private void btnClearMss_Click(object sender, EventArgs e)
+        {
+            txtbox.Clear();
+        }
+    }
+}
+
